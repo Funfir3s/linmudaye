f281d96ba03c44915190f8f34f653442aa3f126c
linmudaye <52953782+linmudaye@users.noreply.github.com>
2021-11-18 11:20:33 +0800

    Update jd_speed_redpocke.js

--- jd_speed_redpocke.js
+++ jd_speed_redpocke.js
@@ -1,9 +1,7 @@
 /*
 京东极速版红包
 自动提现微信现金
-更新时间：2021-8-2
-活动时间：2021-4-6至2021-5-30
-活动地址：https://prodev.m.jd.com/jdlite/active/31U4T6S4PbcK83HyLPioeCWrD63j/index.html
+活动时间：2021-6-1至2021-11-30
 活动入口：京东极速版-领红包
 已支持IOS双京东账号,Node.js支持N个京东账号
 脚本兼容: QuantumultX, Surge, Loon, JSBox, Node.js
@@ -11,17 +9,15 @@
 [task_local]
 #京东极速版红包
 20 0,22 * * * jd_speed_redpocke.js, tag=京东极速版红包, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
-
 ================Loon==============
 [Script]
 cron "20 0,22 * * *" script-path=jd_speed_redpocke.js,tag=京东极速版红包
-
 ===============Surge=================
 京东极速版红包 = type=cron,cronexp="20 0,22 * * *",wake-system=1,timeout=3600,script-path=jd_speed_redpocke.js
-
 ============小火箭=========
 京东极速版红包 = type=cron,script-path=jd_speed_redpocke.js, cronexpr="20 0,22 * * *", timeout=3600, enable=true
 */
+
 const $ = new Env('京东极速版红包');
 const notify = $.isNode() ? require('./sendNotify') : '';
 //Node.js用户请在jdCookie.js处填写京东ck;
@@ -150,7 +146,7 @@ async function sign() {
 function reward_query() {
   return new Promise(resolve => {
     $.get(taskGetUrl("spring_reward_query", {
-      "inviter": ["HXZ60he5XxG8XNUF2LSrZg"][Math.floor((Math.random() * 1))],
+      "inviter": ["TcRo9GSFphN6X-DAuLLTzkXJgjiVoUJzlYLUvgszEh0"][Math.floor((Math.random() * 1))],
       linkId
     }), async (err, resp, data) => {
       try {
@@ -177,7 +173,7 @@ function reward_query() {
 }
 async function redPacket() {
   return new Promise(resolve => {
-    $.get(taskGetUrl("spring_reward_receive",{"inviter":["HXZ60he5XxG8XNUF2LSrZg"][Math.floor((Math.random() * 1))], linkId}),
+    $.get(taskGetUrl("spring_reward_receive",{"inviter":["TcRo9GSFphN6X-DAuLLTzkXJgjiVoUJzlYLUvgszEh0"][Math.floor((Math.random() * 1))], linkId}),
         async (err, resp, data) => {
           try {
             if (err) {
@@ -396,7 +392,7 @@ function cashOut(id,poolBaseId,prizeGroupId,prizeBaseId,) {
 function invite() {
   let t = +new Date()
   let inviterId = [
-    "5V7vHE23qh2EkdBHXRFDuA=="
+    "TcRo9GSFphN6X-DAuLLTzkXJgjiVoUJzlYLUvgszEh0"
   ][Math.floor((Math.random() * 1))]
   var headers = {
     'Host': 'api.m.jd.com',

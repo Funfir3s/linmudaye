cf270c476f7ae4fad84648d058d382326a619e8e
linmudaye <52953782+linmudaye@users.noreply.github.com>
2022-04-18 11:35:32 +0800

    Update jd_jin_tie.js

--- jd_jin_tie.js
+++ jd_jin_tie.js
@@ -12,11 +12,11 @@ const { url } = require('inspector');
 10 0 * * * jd_jin_tie.js, tag=领金贴, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
 ===========Loon===============
 [Script]
-cron "10 0 * * *" script-path=https://raw.githubusercontent.com/linmudaye/linmudaye/main/jd_jin_tie.js,tag=领金贴
+cron "10 0 * * *" script-path=jd_jin_tie.js,tag=领金贴
 =======Surge===========
-领金贴 = type=cron,cronexp="10 0 * * *",wake-system=1,timeout=3600,script-path=https://raw.githubusercontent.com/linmudaye/linmudaye/main/jd_jin_tie.js
+领金贴 = type=cron,cronexp="10 0 * * *",wake-system=1,timeout=3600,script-path=jd_jin_tie.js
 ==============小火箭=============
-领金贴 = type=cron,script-path=https://raw.githubusercontent.com/linmudaye/linmudaye/main/jd_jin_tie.js, cronexpr="10 0 * * *", timeout=3600, enable=true
+领金贴 = type=cron,script-path=jd_jin_tie.js, cronexpr="10 0 * * *", timeout=3600, enable=true
  */
 const $ = new Env('领金贴');
 const notify = $.isNode() ? require('./sendNotify') : '';
@@ -70,7 +70,7 @@ if ($.isNode()) {
 async function main() {
     try {
         await channelUserSignInfo_xh();
-        await queryMission_xh();
+        //await queryMission_xh();
         await channelUserSubsidyInfo_xh();
     } catch (e) {
         $.logErr(e)

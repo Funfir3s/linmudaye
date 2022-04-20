81ed8b7e2d3d9da1440fc2d65dd5baf4dfcf17f9
linmudaye <52953782+linmudaye@users.noreply.github.com>
2022-04-20 12:11:00 +0800

    Update jd_cfd_mooncake.js

--- jd_cfd_mooncake.js
+++ jd_cfd_mooncake.js
@@ -625,7 +625,7 @@ function taskUrl(function_path, body = '', dwEnv = 7) {
       "User-Agent": UA,
       "Accept-Language": "zh-CN,zh-Hans;q=0.9",
       "Referer": "https://st.jingxi.com/",
-      "Cookie": cookie
+      "Cookie": cookie + "cid=4"
     }
   };
 }
@@ -710,7 +710,7 @@ function TotalBean() {
         "User-Agent": "ScriptableWidgetExtension/185 CFNetwork/1312 Darwin/21.0.0",
         "Accept-Language": "zh-CN,zh-Hans;q=0.9",
         "Accept-Encoding": "gzip, deflate, br",
-        "Cookie": cookie
+        "Cookie": cookie + "cid=4"
       }
     }
     $.get(options, (err, resp, data) => {
@@ -796,7 +796,7 @@ async function requestAlgo() {
       'Accept-Language': 'zh-CN,zh;q=0.9,zh-TW;q=0.8,en;q=0.7'
     },
     'body': JSON.stringify({
-      "version": "3.1",
+      "version": "3.0",
       "fp": $.fingerprint,
       "appId": $.appId.toString(),
       "timestamp": Date.now(),
